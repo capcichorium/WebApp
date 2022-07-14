b72af1bd73f9ab639dd28eb01d8fca7014779385
Vlad Gimadiev <vlad.gimadiev@mail.ru>
2022-07-14 19:13:13 +0300

    Add project files.

---
+++ WebApp/Models/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<Order> Orders{ get; set; }
+    public ApplicationContext()
+    {
+        Database.EnsureCreated();
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\webapi;Database=webapi;Trusted_Connection=True;");
+    }
+}
---
+++ WebApp/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string sendersCity { get; set; }
+        public string sendersAdress { get; set; }
+        public string recipientsCity { get; set; }
+        public string recipientsAdress { get; set; }
+        public double Weight { get; set; }
+        public string arrivalDate { get; set; }
+    }
+}
